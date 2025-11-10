f9da35735ac086e7f9f631b7b612628ac0699082
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-11-10 17:31:19 +0300

    More RESTful services

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -17,7 +18,19 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+func (p *Product) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+
+	return e.Encode(p)
+}
+
 type Products []*Product
+
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+
+	return d.Decode(p)
+}
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
@@ -48,4 +61,42 @@ var productList = Products{
 
 func GetProducts() Products {
 	return productList
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+
+	return lp.ID + 1
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
 }
